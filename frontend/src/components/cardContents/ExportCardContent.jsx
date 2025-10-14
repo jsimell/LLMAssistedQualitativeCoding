f97af49189450839cd9fde89d961e72bf7e238fe
Jiri Simell <jiri.simell@gmail.com>
2025-10-14 14:11:59 +0300

    Added navigation buttons to Results and Export steps

--- frontend/src/components/cardContents/ExportCardContent.jsx
+++ frontend/src/components/cardContents/ExportCardContent.jsx
@@ -2,7 +2,11 @@ import { useState, useContext } from "react";
 import { WorkflowContext } from "../../context/WorkflowContext";
 
 const ExportCardContent = () => {
-  
+  return (
+    <div>
+      <StepNavigationButtons hideNext={true}></StepNavigationButtons>
+    </div>
+  );
 }
 
 export default ExportCardContent;
--- frontend/src/components/cardContents/ResultsCardContent.jsx
+++ frontend/src/components/cardContents/ResultsCardContent.jsx
@@ -2,7 +2,11 @@ import { useState, useContext } from "react";
 import { WorkflowContext } from "../../context/WorkflowContext";
 
 const ResultsCardContent = () => {
-  
+  return (
+    <div>
+      <StepNavigationButtons></StepNavigationButtons>
+    </div>
+  );
 }
 
 export default ResultsCardContent;
