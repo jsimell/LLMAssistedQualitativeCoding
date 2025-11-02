ff43618997614bc0b436d18c26566cdec49e0450
Jiri Simell <jiri.simell@gmail.com>
2025-11-02 18:19:03 +0200

    Changed header text

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className="flex w-full h-fit p-7 border-b border-onBackground bg-primary text-white text-3xl font-semibold">
-    QualiCodeAssistant
+    LLM-Assisted Inductive Coding
   </div>
 );
 
