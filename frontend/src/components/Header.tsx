1127953e59b65b0bc7955d2a5fb782de6b26d962
Jiri Simell <jiri.simell@gmail.com>
2025-12-18 01:47:07 +0200

    Updated page title and address

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className="sticky top-0 flex items-center w-full h-fit z-20 p-7 border-b border-onBackground bg-primary text-white text-3xl font-semibold">
-    LLM-Assisted Inductive Coding
+    LLM-Assisted Qualitative Coding
   </div>
 );
 
