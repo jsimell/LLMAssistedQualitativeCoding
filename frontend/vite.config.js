47f1177580e1c38fe0c59bc4046bc3bfb1548d97
Jiri Simell <jiri.simell@gmail.com>
2026-01-07 20:50:52 +0200

    Updated base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LLMAssistedInductiveCoding/',
+  base: '/LLMAssistedQualitativeCoding/',
   plugins: [
     tailwindcss(),
     react({
